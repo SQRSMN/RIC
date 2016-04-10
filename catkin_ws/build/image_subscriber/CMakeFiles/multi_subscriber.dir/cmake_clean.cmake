1beff2ecaefce2eb8f190a22ffa4cbc86763a7f7
simon <simonsimon911@googlemail.com>
2016-04-10 13:24:16 +0200

    _added function_
    -implemented sequential-image prototype in image_subscriber

---
+++ catkin_ws/build/image_subscriber/CMakeFiles/multi_subscriber.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/multi_subscriber.dir/src/multi_subscriber.cpp.o"
+  "/home/simon/catkin_ws/devel/lib/image_subscriber/multi_subscriber.pdb"
+  "/home/simon/catkin_ws/devel/lib/image_subscriber/multi_subscriber"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/multi_subscriber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- catkin_ws/src/image_subscriber/src/image_subscriber.cpp
+++ catkin_ws/src/image_subscriber/src/image_subscriber.cpp
@@ -7,27 +7,30 @@ using namespace ros;
 using namespace cv;
 using namespace std;
 
+Mat matMinus1;
+Mat matNow;
+Mat imageDiff;
+int firstrun = 1;
+
+
 void imageCallback(const sensor_msgs::ImageConstPtr& image)
 {
 sensor_msgs::ImageConstPtr imageInOne_;
 imageInOne_ = image;
 
-sensor_msgs::ImageConstPtr imageInTwo_;
-imageInTwo_ = image;
-
-
 cv_bridge::CvImagePtr imageInOne = cv_bridge::toCvCopy(imageInOne_);
-cv_bridge::CvImagePtr imageInTwo = cv_bridge::toCvCopy(imageInTwo_);
 
-
-Mat imageDifferOne = imageInOne->image;
-Mat imageDifferTwo = imageInTwo->image;
-Mat imageDiff;
-absdiff(imageDifferOne, imageDifferTwo, imageDiff);
+Mat matNow = imageInOne->image;
+if(firstrun == 1){
+matMinus1 = matNow;
+firstrun = 0;
+}
+absdiff(matMinus1, matNow, imageDiff);
+matMinus1 = matNow;
 
 Mat imageOut_ = imageDiff;
     cv::imshow("view", imageOut_);
-//cv::imshow("view", imageDifferOne);
+
 }
 
 int main(int argc, char **argv)
@@ -37,7 +40,9 @@ int main(int argc, char **argv)
   cv::namedWindow("view");
   cv::startWindowThread();
   image_transport::ImageTransport it(nh);
-  image_transport::Subscriber sub = it.subscribe("camera/rgb/image_color", 0.01, imageCallback);
+  image_transport::Subscriber sub = it.subscribe("camera/depth/image", 0.01, imageCallback);
   ros::spin();
   cv::destroyWindow("view");
 }
+
+
